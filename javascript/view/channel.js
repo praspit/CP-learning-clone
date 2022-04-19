0acd82c0c2289d4c8e2466c727406b245c4b2563
Pras Pitasawad <prasrodo@gmail.com>
2022-04-19 12:15:08 +0700

    channels can perform onClick event

--- javascript/view/channel.js
+++ javascript/view/channel.js
@@ -1,9 +1,13 @@
 let channelContainer = document.querySelector(".nav-bar");
 
 export function showAllChannels(channels) {
-    let html = '';
+    channelContainer.innerHTML = '';
+
     channels.forEach(channel=> {
-        html += `<button class="channel-container">${channel.channel_name}</button>`;
+        let btn = document.createElement('button')
+        btn.className = "channel-container";
+        btn.onclick = function(){console.log('hello')};
+        btn.innerHTML = channel.channel_name;
+        channelContainer.appendChild(btn);
     });
-    channelContainer.innerHTML = html;
 }
