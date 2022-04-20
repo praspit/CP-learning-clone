09d38aa841fe82b95130d797d769b9e38f44b424
Pras Pitasawad <prasrodo@gmail.com>
2022-04-20 13:18:29 +0700

    can see answer on each post when click on it and close answer when click again

--- javascript/view/post.js
+++ javascript/view/post.js
@@ -1,4 +1,4 @@
-import { uploadPost } from "../model/post.js";
+import { uploadPost, getOnePostInChannel} from "../model/post.js";
 import { User, Post, Answer, Reply } from "../model/schema.js"
 
 let postsContainer = document.querySelector('.posts-container');
@@ -7,18 +7,69 @@ let postFormContainer = document.querySelector('.post-form-container');
 export function showPostsFromChannel(posts) {
     postsContainer.innerHTML = '';
     posts.forEach(post => {
+        let postAndAnswerContainer = document.createElement('div');
         let postBtn = document.createElement('button');
+
         postBtn.className = 'post-container';
         postBtn.innerHTML = `
             <h1>${post.title}</h1>
             <span>By ${post.author}</span>
             <p>${post.description}</p>
         `
-        postBtn.onclick = function(){
-            console.log(post);
+        postBtn.onclick = async function() {
+            let answerSection = document.querySelector(`[data-uid="${post.uid}"]`).getElementsByClassName("answer-section")[0];
+            if(answerSection.hasAttribute("open")){
+                answerSection.setAttribute("closing","");
+
+                answerSection.addEventListener(
+                    "animationend", 
+                    () => {
+                        answerSection.removeAttribute("closing");
+                        answerSection.removeAttribute("open");
+                    },
+                    { once:true }
+                );
+            }else{
+                try {
+                    let p = await getOnePostInChannel(post.channel_id, post.uid);
+                    console.log(p);
+                    showAllAnswersInOnePost(post.channel_id, post.uid);
+                } catch {
+                    console.log(`can't get post ${post.channel_id} from channel ${post.uid}`)
+                }
+            }
         }
-        postsContainer.appendChild(postBtn);
+
+        let answerSection = document.createElement('div');
+        answerSection.classList = "answer-section";
+
+        postAndAnswerContainer.appendChild(postBtn);
+        postAndAnswerContainer.appendChild(answerSection);
+        postAndAnswerContainer.classList = "post-answer-container"
+        postAndAnswerContainer.dataset.uid = post.uid;
+        postsContainer.appendChild(postAndAnswerContainer);
     });
+}
+
+async function showAllAnswersInOnePost(channel_id, post_id) {
+    let answerSection = document.querySelector(`[data-uid="${post_id}"]`).getElementsByClassName("answer-section")[0];
+    answerSection.innerHTML = '';
+    let post = await getOnePostInChannel(channel_id, post_id);
+    if(post.answers.length===0)return;
+    post.answers.forEach(answer => {
+        let answerContainer = document.createElement('div');
+        answerContainer.classList = "answer-container";
+        answerContainer.innerHTML = `
+            <div>
+                ${answer.author}
+            </div>
+            <div>
+                ${answer.content}
+            </div>
+        `
+        answerSection.appendChild(answerContainer);
+    })
+    answerSection.setAttribute("open","");
 }
 
 export function showPostForm(channel_id) {
