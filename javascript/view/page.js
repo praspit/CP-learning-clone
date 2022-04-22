7f88db69a36672e0843927920f69dc35dff975af
bbompk <panithiv@gmail.com>
2022-04-23 03:17:47 +0700

    from create go back to login button

--- javascript/view/page.js
+++ javascript/view/page.js
@@ -1,4 +1,4 @@
-import { goToContentPage, goToCreateUserPage } from '../controller/pageCtrl.js';
+import { goToContentPage, goToCreateUserPage, goToLandingPage } from '../controller/pageCtrl.js';
 import { getUser, uploadNewUser } from '../model/user.js';
 import {valid_name, valid_tag, generate_tag} from '../utility/tools.js';
 import { User, Post, Answer, Reply } from "../model/schema.js"
@@ -14,6 +14,7 @@ export function initializeLandingPage() {
             #
             <input type="text" id="landing-page-tag-input" name="tag" placeholder="tag" maxlength=4>
         </div>
+        <div class="login-error-container" id="login-error"></div>
     </div>
     `;
 
@@ -32,9 +33,11 @@ export function initializeLandingPage() {
                 document.getElementsByClassName('welcome-user')[0].innerHTML = `<h2>Welcome, ${userExist.username}</h2>`
                 goToContentPage();
             }else{
+                document.getElementById('login-error').innerText = 'user doesn\'t exist!';
                 console.log("user doesn't exist!");
             }
         }else{
+            document.getElementById('login-error').innerText = 'user doesn\'t exist!';
             console.log('invalid username');
         }
     }
@@ -58,6 +61,7 @@ export function initializeCreateUserPage() {
     createUserFormContainer.classList = 'create-user-form-container';
     createUserFormContainer.innerHTML = `
         <input type="text" id="create-user-page-username-input" name="name" placeholder="name" maxlength=20>
+        <div class="login-error-container" id="create-user-error"></div>
     `
 
     let createUserBtn = document.createElement('button');
@@ -74,13 +78,28 @@ export function initializeCreateUserPage() {
                 document.getElementsByClassName('welcome-user')[0].innerHTML = `<h2>Welcome ${user.username}</h2>`
                 goToContentPage();
             }else {
+                
                 console.log('tag collision');   
             }
         }else{
+            document.getElementById('create-user-error').innerText = 'invalid username';
             console.log('invalid username');
         }
     }
     createUserFormContainer.appendChild(createUserBtn);
+
+    let ORText = document.createElement('h4');
+    ORText.innerText = 'OR';
+    createUserFormContainer.appendChild(ORText);
+
+    let goToLandingPageBtn = document.createElement('button');
+    goToLandingPageBtn.innerText = 'back';
+    goToLandingPageBtn.classList = 'back-to-login-btn';
+    goToLandingPageBtn.onclick = function() {
+        goToLandingPage();
+        createUserPage.innerHTML = '';
+    }
+    createUserFormContainer.appendChild(goToLandingPageBtn);
 
     createUserPage.appendChild(createUserFormContainer);
 }
